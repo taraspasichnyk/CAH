875e1a5c873fe9987d5668776dcd2282bbfd83c5
taras.yurkiv <taras.yurkiv@eleks.com>
2023-02-07 12:27:26 +0200

    merged conflicts

--- androidApp/src/main/java/com/eleks/cah/android/MainActivity.kt
+++ androidApp/src/main/java/com/eleks/cah/android/MainActivity.kt
@@ -3,12 +3,11 @@ package com.eleks.cah.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
@@ -16,11 +15,7 @@ import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.eleks.cah.game.GameViewModel
 import com.eleks.cah.init
-import com.eleks.cah.menu.MenuContract
-import com.eleks.cah.menu.MenuViewModel
-import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : ComponentActivity() {
 

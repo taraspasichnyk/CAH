aef4e638e63f008526ae1d0d77e3fa58ce81c2a3
taras.yurkiv <taras.yurkiv@eleks.com>
2023-02-09 14:20:14 +0200

    fixed ScopedViewModel.kt(removed unnecessary scope.cancel() call) removd UiEvent from BaseViewModel

--- shared/src/androidMain/kotlin/com/eleks/cah/base/ScopedViewModel.kt
+++ shared/src/androidMain/kotlin/com/eleks/cah/base/ScopedViewModel.kt
@@ -3,14 +3,8 @@ package com.eleks.cah.base
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancel
 
 actual abstract class ScopedViewModel : ViewModel() {
     actual val scope: CoroutineScope
         get() = viewModelScope
-
-    override fun onCleared() {
-        super.onCleared()
-        scope.cancel()
-    }
 }
--- shared/src/commonMain/kotlin/com/eleks/cah/menu/MenuContract.kt
+++ shared/src/commonMain/kotlin/com/eleks/cah/menu/MenuContract.kt
@@ -1,17 +1,9 @@
 package com.eleks.cah.menu
 
 import com.eleks.cah.base.UiEffect
-import com.eleks.cah.base.UiEvent
 import com.eleks.cah.base.UiState
 
 interface MenuContract {
-    sealed class Event : UiEvent {
-        object StartNewGame : Event()
-        object JoinToExistingGame : Event()
-        object StartSettings : Event()
-        object Exit : Event()
-    }
-
     data class State(
         private val isLoading: Boolean = false
     ) : UiState

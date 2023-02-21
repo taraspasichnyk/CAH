b75c9d0b231392bcb0c824449dbc42ab056d57b8
OleksandrHrebenko <oleksandr.hrebenko@eleks.com>
2023-02-21 19:35:32 +0200

    cah-13 add button next with bottom panel

--- androidApp/src/main/java/com/eleks/cah/android/user_cards/UserCardsScreen.kt
+++ androidApp/src/main/java/com/eleks/cah/android/user_cards/UserCardsScreen.kt
@@ -1,14 +1,16 @@
 package com.eleks.cah.android.user_cards
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -19,10 +21,10 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.eleks.cah.android.AppTheme
 import com.eleks.cah.android.MyApplicationTheme
 import com.eleks.cah.android.R
@@ -36,7 +38,7 @@ import com.eleks.cah.android.widgets.GameLabelSize
 
 @Composable
 fun UserCardsScreen() {
-
+    // TODO remove hardcode data
     val cards: List<Card> = listOf(
         Card("Божеволіти від нестримного програмування"),
         Card("Божеволіти від нестримного програмування"),
@@ -61,53 +63,73 @@ fun UserCardsScreen() {
     val userCards = remember {
         cards.toMutableStateList()
     }
+    Box(modifier = Modifier.fillMaxSize()) {
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        GameHeader(
-            gameLabelSize = GameLabelSize.SMALL, headerHeight = AppTheme.dimens.headerSize
-        )
-
-        Spacer(
-            modifier = Modifier.height(AppTheme.dimens.sizeMedium)
-        )
-
-        Text(
-            text = stringResource(id = R.string.user_cards),
-            textAlign = TextAlign.Center,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            style = txtSemibold24,
-            color = Color.Black
-        )
-
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(COLUMN_COUNT),
-            verticalArrangement = Arrangement.spacedBy(AppTheme.dimens.sizeSmall),
-            contentPadding = PaddingValues(
-                start = AppTheme.dimens.sizeMedium,
-                end = AppTheme.dimens.sizeMedium,
-                top = AppTheme.dimens.sizeMedium,
-                bottom = AppTheme.dimens.sizeSmall
-            ),
-            modifier = Modifier.weight(1f)
+        Column(
+            modifier = Modifier.fillMaxSize()
         ) {
-            items(userCards) {
-                UserCardItemView(it)
+            GameHeader(
+                gameLabelSize = GameLabelSize.SMALL, headerHeight = AppTheme.dimens.headerSize
+            )
+
+            Spacer(
+                modifier = Modifier.height(AppTheme.dimens.sizeMedium)
+            )
+
+            Text(
+                text = stringResource(id = R.string.user_cards),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = AppTheme.dimens.sizeSmall),
+                style = txtSemibold24,
+                color = Color.Black
+            )
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(COLUMN_COUNT),
+                verticalArrangement = Arrangement.spacedBy(AppTheme.dimens.sizeSmall),
+                contentPadding = PaddingValues(
+                    start = AppTheme.dimens.sizeMedium,
+                    end = AppTheme.dimens.sizeMedium,
+                    top = AppTheme.dimens.sizeSmall,
+                    bottom = AppTheme.dimens.sizeSmall
+                ),
+                modifier = Modifier.weight(1f)
+            ) {
+                items(userCards) {
+                    UserCardItemView(it)
+                }
+            }
+
+            Spacer(
+                modifier = Modifier.defaultMinSize(
+                    minHeight = dimensionResource(id = R.dimen.padding_80)
+                )
+            )
+
+            CardBackground(
+                R.drawable.bg_pattern_big, modifier = Modifier.fillMaxWidth()
+            ) {
+                Spacer(
+                    modifier = Modifier
+                        .navigationBarsPadding()
+                        .height(dimensionResource(id = R.dimen.padding_44))
+                )
             }
         }
 
-        BlackButton(onClick = {
-            //
-        }, text = R.string.label_next)
-
-        CardBackground(R.drawable.bg_pattern_big, modifier = Modifier.fillMaxWidth()) {
-            Spacer(
-                modifier = Modifier
-                    .navigationBarsPadding()
-                    .height(44.dp)
-            )
+        Box(
+            contentAlignment = Alignment.CenterEnd, modifier = Modifier
+                .padding(
+                    end = AppTheme.dimens.sizeMedium,
+                    bottom = dimensionResource(id = R.dimen.padding_70)
+                )
+                .align(Alignment.BottomEnd)
+        ) {
+            BlackButton(onClick = {
+                // click function next
+            }, text = R.string.label_next)
         }
     }
 }

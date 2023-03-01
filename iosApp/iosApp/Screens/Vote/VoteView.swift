e09f1a9bdc9e67b3f4a13a1e0143f49c7af0ee69
Taras Pasichnyk <taras.pasichnyk@eleks.com>
2023-03-01 17:14:31 +0200

    feat: create vote view and basic logic implementation

--- iosApp/iosApp/Screens/Vote/VoteView.swift
+++ iosApp/iosApp/Screens/Vote/VoteView.swift
@@ -14,7 +14,8 @@ struct VoteView<ViewModel: GameModelProtocol>: View {
 
     @ObservedObject var viewModel: ViewModel
 
-    @State var selectedValue: Int = 0
+    @State var displayedCardIndex: Int = 0
+    @State var selectedRateValue: Int = 0
 
     // MARK: - Lifecycle
 
@@ -24,11 +25,56 @@ struct VoteView<ViewModel: GameModelProtocol>: View {
                 Text("Раунд \(viewModel.round?.number ?? 0) - Голосування")
                     .padding(.top, .larger)
                     .font(.titleSemiBold)
-                Spacer()
-                RateView($selectedValue) { newValue in
-                    selectedValue = newValue
+                ZStack {
+                    VStack {
+                        SelectorView($displayedCardIndex, count: viewModel.round?.playerCards.count ?? 0)
+                            .padding(.top, 16.0)
+                            .padding([.leading, .trailing], 16.0)
+                        if let question = viewModel.round?.questionCard {
+                            ZStack(alignment: .bottom) {
+                                VStack {
+                                    QuestionCardView(question: question.question)
+                                    Spacer()
+                                }
+                                if let answers = viewModel.round?.playerCards[displayedCardIndex].playerAnswers {
+                                    VStack {
+                                        Spacer()
+                                        AnswerCardView(answer: answers[viewModel.round?.number ?? 0])
+                                            .font(.cardSmall)
+                                            .frame(width: 124, height: 168)
+                                            .rotationEffect(.degrees(-8))
+                                            .offset(x: -16.0)
+                                    }
+                                }
+                            }
+                            .padding(.top, .larger)
+                            .padding(.bottom, .large)
+                        }
+                        Spacer()
+                    }
+                    HStack {
+                        Rectangle()
+                            .opacity(0.001)
+                            .onTapGesture {
+                                // TODO: Pack logic into VM
+                                if displayedCardIndex > 0 {
+                                    displayedCardIndex-=1
+                                }
+                            }
+                        Rectangle()
+                            .opacity(0.001)
+                            .onTapGesture {
+                                // TODO: Pack logic into VM
+                                if displayedCardIndex < (viewModel.round?.playerCards.count ?? 0) - 1 {
+                                    displayedCardIndex+=1
+                                }
+                            }
+                    }
                 }
-                .padding(.bottom, .extraLarge)
+                RateView($selectedRateValue) { newValue in
+                    selectedRateValue = newValue
+                }
+                .padding(.bottom, 40.0)
             }
         }
         .ignoresSafeArea()

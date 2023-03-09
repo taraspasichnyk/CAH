78f1ec9253c4f9a4f2daefc61aa14308de8aa01d
Artem Yelizarov <artem.yelizarov@eleks.com>
2023-03-09 15:34:13 +0200

    Fix indentation in vote view

--- iosApp/iosApp/Screens/Vote/VoteView.swift
+++ iosApp/iosApp/Screens/Vote/VoteView.swift
@@ -20,88 +20,88 @@ struct VoteView<ViewModel: VoteViewModelProtocol>: View {
 
     var body: some View {
         ContainerView(header: .small) {
-                VStack {
-                    Text("Раунд \(viewModel.roundNumber) - Голосування")
-                        .padding(.top, .larger)
-                        .font(.titleSemiBold)
-                    SelectorView(viewModel.displayedAnswerIndex, count: viewModel.answers.count)
-                        .padding(.top, .medium)
-                        .padding([.leading, .trailing], .medium)
-                    ZStack {
-                        HStack {
-                            Rectangle()
-                                .opacity(0.001)
-                                .onTapGesture {
-                                    viewModel.previousAnswer()
-                                    answerOnTop = true
-                                }
-                            Rectangle()
-                                .opacity(0.001)
-                                .onTapGesture {
-                                    viewModel.nextAnswer()
-                                    answerOnTop = true
-                                }
-                        }
-                        ZStack(alignment: .bottom) {
-                            VStack {
-                                QuestionCardView(question: viewModel.question)
-                                    .font(.cardSmall)
-                                    .frame(width: 124)
-                                    .offset(
-                                        x: isOut ? 0 : 20,
-                                        y: isOut ? 0 : -20
-                                    )
-                                Spacer()
+            VStack {
+                Text("Раунд \(viewModel.roundNumber) - Голосування")
+                    .padding(.top, .larger)
+                    .font(.titleSemiBold)
+                SelectorView(viewModel.displayedAnswerIndex, count: viewModel.answers.count)
+                    .padding(.top, .medium)
+                    .padding([.leading, .trailing], .medium)
+                ZStack {
+                    HStack {
+                        Rectangle()
+                            .opacity(0.001)
+                            .onTapGesture {
+                                viewModel.previousAnswer()
+                                answerOnTop = true
                             }
-                            .zIndex(answerOnTop ? 0 : 1)
-                            VStack {
-                                Spacer()
-                                if let answer = viewModel.displayedAnswer.playerAnswers.first {
-                                    ZStack(alignment: .bottom) {
-                                        AnswerCardView(answer: answer.text)
-                                            .font(.cardSmall)
-                                            .frame(width: 124)
-                                        if let value = RateView.Value(rawValue: viewModel.displayedAnswer.score) {
-                                            value.image.resizable()
-                                                .aspectRatio(1.0, contentMode: .fit)
-                                                .frame(width: 52)
-                                                .padding(.bottom, .large)
-                                        }
+                        Rectangle()
+                            .opacity(0.001)
+                            .onTapGesture {
+                                viewModel.nextAnswer()
+                                answerOnTop = true
+                            }
+                    }
+                    ZStack(alignment: .bottom) {
+                        VStack {
+                            QuestionCardView(question: viewModel.question)
+                                .font(.cardSmall)
+                                .frame(width: 124)
+                                .offset(
+                                    x: isOut ? 0 : 20,
+                                    y: isOut ? 0 : -20
+                                )
+                            Spacer()
+                        }
+                        .zIndex(answerOnTop ? 0 : 1)
+                        VStack {
+                            Spacer()
+                            if let answer = viewModel.displayedAnswer.playerAnswers.first {
+                                ZStack(alignment: .bottom) {
+                                    AnswerCardView(answer: answer.text)
+                                        .font(.cardSmall)
+                                        .frame(width: 124)
+                                    if let value = RateView.Value(rawValue: viewModel.displayedAnswer.score) {
+                                        value.image.resizable()
+                                            .aspectRatio(1.0, contentMode: .fit)
+                                            .frame(width: 52)
+                                            .padding(.bottom, .large)
                                     }
-                                    .rotationEffect(.degrees(-8))
-                                    .offset(
-                                        x: isOut ? -20 : -40,
-                                        y: isOut ? 0 : 20
-                                    )
                                 }
+                                .rotationEffect(.degrees(-8))
+                                .offset(
+                                    x: isOut ? -20 : -40,
+                                    y: isOut ? 0 : 20
+                                )
                             }
-                            .zIndex(answerOnTop ? 1 : 0)
                         }
-                        .padding(.top, .larger)
-                        .padding(.bottom, .large)
-                        .onTapGesture {
-                            let duration = 0.14
-                            withAnimation(.easeIn(duration: duration)) {
-                                isOut.toggle()
+                        .zIndex(answerOnTop ? 1 : 0)
+                    }
+                    .padding(.top, .larger)
+                    .padding(.bottom, .large)
+                    .onTapGesture {
+                        let duration = 0.14
+                        withAnimation(.easeIn(duration: duration)) {
+                            isOut.toggle()
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+                            withAnimation(.easeOut(duration: duration)) {
+                                answerOnTop.toggle()
                             }
                             DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
                                 withAnimation(.easeOut(duration: duration)) {
-                                    answerOnTop.toggle()
-                                }
-                                DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-                                    withAnimation(.easeOut(duration: duration)) {
-                                        isOut.toggle()
-                                    }
+                                    isOut.toggle()
                                 }
                             }
                         }
                     }
-                    RateView(viewModel.displayedAnswer.score) { newValue in
-                        viewModel.voteForCard(score: newValue)
-                    }
-                    .padding(.bottom, 40.0)
-                    .padding([.leading, .trailing], 40.0)
                 }
+                RateView(viewModel.displayedAnswer.score) { newValue in
+                    viewModel.voteForCard(score: newValue)
+                }
+                .padding(.bottom, 40.0)
+                .padding([.leading, .trailing], 40.0)
+            }
         }
         .ignoresSafeArea(edges: .bottom)
     }
